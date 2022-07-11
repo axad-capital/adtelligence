e964660bfe157329367a47cb63106c4e96545d9f
Anthony Parrino <anthonyparrino90@gmail.com>
2022-07-11 11:50:14 -0400

    started on nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomeComp from './components/HomeComp/HomeComp';
+import NavComp from './components/Nav/NavComp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavComp />
+        <Routes>
+          <Route exact path='/' element={<HomeComp />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/Nav/NavComp.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './navComp.css'
+
+const NavComp = () => {
+    return (
+        <div>
+            <div className='nav-container'>
+                <div className='logo-container'>
+                    <img className='nav-logo' src="/images/adtelligencelogo.png" alt="" />
+                </div>
+
+                <div className='nav-list-container'>
+
+                </div>
+
+                <div className='nav-button-container'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NavComp
